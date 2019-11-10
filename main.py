229c078579874c3284386066479690722b3cf101
Ainsley Rutterford <ar16478@my.bristol.ac.uk>
2019-11-10 12:30:03 +0000

    Add neurons to network()

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ import tiny
 
 nn = tiny.network(activation='sigmoid')
 nn.add_layer(3, 5)
-nn.add_layer(5, 1)
+nn.add_layer(5, 2)
--- tiny.py
+++ tiny.py
@@ -2,6 +2,7 @@ import numpy as np
 
 class network:
 
+    neurons = []
     weights = []
     activation   = lambda: None
     activation_d = lambda: None
@@ -18,16 +19,18 @@ class network:
         return x * (1 - x)
 
     def add_layer(self, inputs, outputs):
+        self.neurons.append([0] * outputs)
         self.weights.append(np.random.rand(inputs, outputs))
+
+    def forward_prop(self, inputs):
+        for i, layer in enumerate(self.weights):
+            layer_outputs = []
+            for j, neuron_weights in enumerate(layer.T):
+                activation = self.activation(np.dot(neuron_weights, inputs))
+                self.neurons[i][j] = activation
+                layer_outputs.append(activation)
+            inputs = layer_outputs
+        return inputs
 
     def train(self):
         pass
-
-    def ponder(self, inputs):
-        for layer in self.weights:
-            layer_outputs = []
-            for neuron_weights in layer.T:
-                weighted_sum = np.dot(neuron_weights, inputs)
-                layer_outputs.append(self.activation(weighted_sum))
-            inputs = layer_outputs
-        return inputs
