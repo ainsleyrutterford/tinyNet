72c891be6f1faa3477174a464eb0819904bc2bc9
Ainsley Rutterford <ar16478@my.bristol.ac.uk>
2019-11-16 16:54:16 +0000

    Update mnist.py to include simple network

--- mnist.py
+++ mnist.py
@@ -2,8 +2,18 @@ import idx2numpy
 import numpy as np
 import tiny
 
-training_images = idx2numpy.convert_from_file('mnist/train-images-idx3-ubyte')[:10000]
-training_labels = idx2numpy.convert_from_file('mnist/train-labels-idx1-ubyte')[:10000]
-training_images = training_images.reshape(10000,784)
+np.seterr(over='ignore')
+
+samples = 1000
+
+training_images = idx2numpy.convert_from_file('mnist/train-images-idx3-ubyte')[:samples]
+training_labels = idx2numpy.convert_from_file('mnist/train-labels-idx1-ubyte')[:samples]
+training_images = training_images.reshape(samples,784)
 
 training_data = np.c_[ training_images, training_labels ]
+
+nn = tiny.network(activation='sigmoid')
+nn.add_layer(784, 200)
+nn.add_layer(200, 10)
+
+nn.train(training_data, 0.7, 1000)
