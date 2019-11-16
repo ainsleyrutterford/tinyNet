f51885aedefe470b72630dbdfefeb44876c3ae81
Ainsley Rutterford <ar16478@my.bristol.ac.uk>
2019-11-16 13:02:24 +0000

    Reimplement add_layer() and forward_prop()

--- main.py
+++ main.py
@@ -6,12 +6,10 @@ data = [[0, 0, 0], # AND
         [1, 1, 1]]
 
 nn = tiny.network(activation='sigmoid')
-nn.add_layer(2, 4)
-nn.add_layer(4, 1)
+nn.add_layer(2, 1)
+nn.add_layer(1, 2)
 
-nn.train(data, 1, 20000)
+print([neuron.weights for neuron in nn.neurons[0]])
+print([neuron.weights for neuron in nn.neurons[1]])
 
-print(nn.forward_prop([0,0]))
-print(nn.forward_prop([0,1]))
-print(nn.forward_prop([1,0]))
 print(nn.forward_prop([1,1]))
--- tiny.py
+++ tiny.py
@@ -1,13 +1,23 @@
 import numpy as np
 
 class neuron:
+
+    weights = []
     activation = 0
     delta = 0
+
+    def __init__(self, inputs):
+        self.weights = np.random.uniform(-1, 1, (inputs))
+        # if (inputs == 1):
+        #     self.weights = np.array([0.13436424411240122, 0.8474337369372327, 0.763774618976614])
+        # elif (inputs == 2):
+        #     self.weights = np.array([0.2550690257394217, 0.49543508709194095])
+        # elif (inputs == 3):
+        #     self.weights = np.array([0.4494910647887381, 0.651592972722763])
 
 class network:
 
     neurons = []
-    weights = []
     activation   = lambda: None
     activation_d = lambda: None
 
@@ -23,51 +33,19 @@ class network:
         return x * (1 - x)
 
     def add_layer(self, inputs, outputs):
-        self.neurons.append([neuron()] * outputs)
-        self.weights.append(np.random.uniform(-1, 1, (inputs, outputs)))
+        self.neurons.append([neuron(inputs + 1) for i in range(outputs)])
+        # if (outputs == 1):
+        #     self.neurons.append([neuron(1)])
+        # elif (outputs == 2):
+        #     self.neurons.append([neuron(2),neuron(3)])
 
     def forward_prop(self, inputs):
-        for i, layer in enumerate(self.weights):
+        for layer in self.neurons:
             layer_outputs = []
-            for j, neuron_weights in enumerate(layer.T):
-                activation = self.activation(np.dot(neuron_weights, inputs))
-                self.neurons[i][j].activation = activation
+            for neuron in layer:
+                bias = neuron.weights[-1]
+                activation = self.activation(np.dot(neuron.weights[:-1], inputs) + bias)
+                neuron.activation = activation
                 layer_outputs.append(activation)
             inputs = layer_outputs
         return inputs
-    
-    def back_prop(self, expected):
-        for i, layer_neurons in enumerate(reversed(self.neurons)):
-            errors = []
-            if (i == 0):
-                for j, neuron in enumerate(layer_neurons):
-                    errors.append(expected[j] - neuron.activation)
-            else:
-                for j in range(len(layer_neurons)):
-                    error = 0.0
-                    next_index = len(self.weights) - i
-                    for k in range(len(self.weights[next_index][j])):
-                        error += (self.weights[next_index][j][k] * self.neurons[next_index][k].delta)
-                    errors.append(error) 
-            for j, neuron in enumerate(layer_neurons):
-                neuron.delta = errors[j] * self.activation_d(neuron.activation)
-
-    def update_weights(self, inputs, learning_rate):
-        for i in range(len(self.weights)):
-            if (i != 0):
-                inputs = [neuron.activation for neuron in self.neurons[i - 1]]
-            for n in range(len(self.neurons[i])):
-                for j in range(len(inputs)):
-                    self.weights[i][j][n] += learning_rate * self.neurons[i][n].delta * inputs[j]
-
-    def train(self, data, learning_rate, epochs):
-        for epoch in range(epochs):
-            sum_error = 0
-            for sample in data:
-                outputs = self.forward_prop(sample[:-1])
-                label = sample[-1]
-                expected_outputs = [label] * len(self.neurons[-1])
-                sum_error += sum([(expected_outputs[i] - outputs[i])**2 for i in range(len(expected_outputs))])
-                self.back_prop(expected_outputs)
-                self.update_weights(sample[:-1], learning_rate)
-            # print(f'epoch {epoch}, error {sum_error}')
